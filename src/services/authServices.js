512aaaae047315787519b7209a7849b74c250f68
Ignacio Giadach <igiadach@gmail.com>
2020-07-23 17:00:59 +1000

    login connected to server with axios
    Co-authored-by: Luisa Ziccarelli <luisaziccarelli@gmail.com>

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,9 +1,23 @@
+import api from '../config/api'
+
 export function getUserFromSessionStorage() {
-    return localStorage.getItem("loggedInUser")
+    return sessionStorage.getItem("loggedInUser")
 }
 
 export function setUserInSessionStorage(user) {
-    user ? localStorage.setItem("loggedInUser", user)
-        : sessionStorage.removeItem("loggedInUser")
+    user ? sessionStorage.setItem("loggedInUser", user)
+         : sessionStorage.removeItem("loggedInUser")
 }
+
+export async function loginUser(userInfo) {
+    // call to server to login user
+    // return user info if successful and error if not
+    const response = await api.post("/auth/login", userInfo)
+    console.log("got user back from server", response) 
+    return response.data
+}
+
+
+
+
 
