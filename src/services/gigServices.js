6549712f1c78383aeea19586136f0c736fb3d9e5
Ignacio Giadach <igiadach@gmail.com>
2020-07-24 13:40:57 +1000

    Gig, gigServices and App.js
    Co-authored-by: Luisa Ziccarelli <luisaziccarelli@gmail.com>

--- src/services/gigServices.js
+++ src/services/gigServices.js
@@ -1,5 +1,12 @@
 import api from '../config/api'
 
+export async function getEventFromId(gigs,id) {
+  
+    const event =  gigs.find((event) =>  event._id === id)
+    console.log(event)
+    return event
+}
+
 export async function getAllEvents() {
     const response = await api.get("/events")
     return response.data
