aeb2ae73219c16f3a263f24376186b328eae2b11
Ignacio Giadach <igiadach@gmail.com>
2020-07-31 11:12:54 +1000

    connect to heroku

--- src/config/api.js
+++ src/config/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-// const deployedServer = "https://secret-gigs.herokuapp.com"
+const deployedServer = "https://secret-gigs.herokuapp.com"
 
 
 //create an axios instance 
 export default axios.create(
     {
-        baseURL: /*deployedServer || */'http://localhost:3003',
+        baseURL: deployedServer || 'http://localhost:3003',
         timeout: 5000,
         withCredentials: true
 
